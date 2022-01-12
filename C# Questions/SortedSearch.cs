39d11b67bddc83b664d5cf15f94bc6548d4d58c8
Debra J <debra321@hotmail.com>
2022-01-12 10:18:59 +0800

    Add files via upload

---
+++ C# Questions/SortedSearch.cs
@@ -0,0 +1,43 @@
+﻿/*
+Implement function CountNumbers that accepts a sorted array of unique integers and, efficiently with respect to time used, counts the number of array elements that are less than the parameter lessThan.
+
+For example, SortedSearch.CountNumbers(new int[] { 1, 3, 5, 7 }, 4) should return 2 because there are two array elements less than 4. 
+ */
+
+using System;
+
+public class SortedSearch
+{
+    public static int CountNumbers(int[] sortedArray, int lessThan)
+    {
+        int left = 0;
+        int right = sortedArray.Length - 1;
+        int floor = -1;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            if(sortedArray[mid]== lessThan)
+            {
+                while(mid >= 0 && sortedArray[mid] == lessThan ) 
+                    mid--;
+                if (mid == -1) return 0;
+                else return mid + 1;
+            }
+            if (sortedArray[mid] > lessThan)
+                right = mid- 1;
+            else
+            {
+                left = mid + 1;
+                floor = mid; //ref: https://www.techiedelight.com/find-floor-ceil-number-sorted-array/
+            }
+        }
+        if (floor == -1)
+            return -1;
+        else
+            return floor + 1;
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(SortedSearch.CountNumbers(new int[] { 1, 3,4, 5, 6,7 }, 4));
+    }
+}
---
+++ C# Questions/TwoSum.cs
@@ -0,0 +1,38 @@
+﻿/*
+Write a function that, when passed a list and a target sum, returns, efficiently with respect to time used, two distinct zero-based indices of any two of the numbers, whose sum is equal to the target sum. If there are no two numbers, the function should return null.
+
+For example, FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10) should return a Tuple<int, int> containing any of the following pairs of indices:
+
+0 and 3 (or 3 and 0) as 3 + 7 = 10
+1 and 5 (or 5 and 1) as 1 + 9 = 10
+2 and 4 (or 4 and 2) as 5 + 5 = 10
+*/
+
+using System;
+using System.Collections.Generic;
+
+class TwoSum
+{
+    public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
+    {
+        Dictionary<int,int> map = new Dictionary<int, int>();
+        for (int i = 0; i < list.Count; i++)
+        {            
+            int target = sum - list[i];
+            if (map.ContainsKey(target))
+                return new Tuple<int, int>(i, map[target]);
+            map[list[i]] = i;
+
+        }
+        return null;
+    }
+
+    public static void Main(string[] args)
+    {
+        Tuple<int, int> indices = FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10);
+        if (indices != null)
+        {
+            Console.WriteLine(indices.Item1 + " " + indices.Item2);
+        }
+    }
+}
