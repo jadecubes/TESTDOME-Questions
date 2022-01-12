e9d62fddca787208957c2a856d0509f528411eab
Debra J <debra321@hotmail.com>
2022-01-12 10:10:06 +0800

    Update BST.cs

--- C# Questions/BST.cs
+++ C# Questions/BST.cs
@@ -1,6 +1,4 @@
-﻿using System;
 /*
- * 
 A three-node binary tree.Binary search tree (BST) is a binary tree where the value of each node is larger or equal to the values in all the nodes in that node's left subtree and is smaller than the values in all the nodes in that node's right subtree.
 
 Write a function that, efficiently with respect to time used, checks if a given binary search tree contains a given value.
@@ -11,8 +9,10 @@ n1 (Value: 1, Left: null, Right: null)
 n2 (Value: 2, Left: n1, Right: n3)
 n3 (Value: 3, Left: null, Right: null)
 Call to Contains(n2, 3) should return true since a tree with root at n2 contains number 3.
- * 
  */
+
+using System;
+
 public class Node
 {
     public int Value { get; set; }
