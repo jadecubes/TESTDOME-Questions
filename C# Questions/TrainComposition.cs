64a7251b586e982022983a948f5d2cd2ea90923b
Debra J <debra321@hotmail.com>
2022-01-12 10:13:36 +0800

    Update TrainComposition.cs

--- C# Questions/TrainComposition.cs
+++ C# Questions/TrainComposition.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
 /*
- * 
 A TrainComposition is built by attaching and detaching wagons from the left and the right sides, efficiently with respect to time used.
 
 For example, if we start by attaching wagon 7 from the left followed by attaching wagon 13, again from the left, we get a composition of two wagons (13 and 7 from left to right). Now the first wagon that can be detached from the right is 7 and the first that can be detached from the left is 13.
 
 Implement a TrainComposition that models this problem.
- *
  */
+
+
+using System;
+using System.Collections.Generic;
+
 public class Wagon
 {
     public Wagon nxt { get; set; }
