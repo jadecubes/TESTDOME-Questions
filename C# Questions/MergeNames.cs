32a5835e01eab3aeb3338ce704e86d38df9ea54d
Debra J <debra321@hotmail.com>
2022-01-12 10:12:05 +0800

    Update MergeNames.cs

--- C# Questions/MergeNames.cs
+++ C# Questions/MergeNames.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
 /*
- * 
 Implement the UniqueNames method. When passed two arrays of names, it will return an array containing the names that appear in either or both arrays. The returned array should have no duplicates.
 
 For example, calling MergeNames.UniqueNames(new string[]{'Ava', 'Emma', 'Olivia'}, new string[]{'Olivia', 'Sophia', 'Emma'}) should return an array containing Ava, Emma, Olivia, and Sophia in any order.
- * 
  */
+
+using System;
+using System.Collections.Generic;
+
 public class MergeNames
 {
     public static string[] UniqueNames(string[] names1, string[] names2)
