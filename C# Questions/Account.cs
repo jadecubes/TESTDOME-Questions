08d1e922b5c61dd806906b7972c45e06832e4e94
Debra J <debra321@hotmail.com>
2022-01-12 10:09:29 +0800

    Update Account.cs

--- C# Questions/Account.cs
+++ C# Questions/Account.cs
@@ -1,4 +1,17 @@
-﻿using System;
+/*
+Each account on a website has a set of access flags that represent a users access.
+
+Update and extend the enum so that it contains three new access flags:
+
+A Writer access flag that is made up of the Submit and Modify flags.
+An Editor access flag that is made up of the Delete, Publish and Comment flags.
+An Owner access that is made up of the Writer and Editor flags.
+For example, the code below should print "False" as the Writer flag does not contain the Delete flag.
+
+Console.WriteLine(Access.Writer.HasFlag(Access.Delete))
+*/
+
+using System;
 
 public class Account
 {
