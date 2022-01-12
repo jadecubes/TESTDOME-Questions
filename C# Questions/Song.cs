d8bba09723045fea90fb98f700ea55ef196bd6fa
Debra J <debra321@hotmail.com>
2022-01-12 10:13:06 +0800

    Update Song.cs

--- C# Questions/Song.cs
+++ C# Questions/Song.cs
@@ -1,6 +1,4 @@
-﻿using System;
 /*
- * 
 A playlist is considered a repeating playlist if any of the songs contain a reference to a previous song in the playlist. Otherwise, the playlist will end with the last song which points to null.
 
 Implement a function IsRepeatingPlaylist that, efficiently with respect to time used, returns true if a playlist is repeating or false if it is not.
@@ -14,8 +12,10 @@ first.NextSong = second;
 second.NextSong = first;
 
 Console.WriteLine(first.IsRepeatingPlaylist());
- * 
  */
+
+using System;
+
 public class Song
 {
     private string name;
