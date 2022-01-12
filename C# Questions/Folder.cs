4d41b482ec6f8ade946f038d59b2f9ace75cfb4d
Debra J <debra321@hotmail.com>
2022-01-12 10:11:01 +0800

    Update Folder.cs

--- C# Questions/Folder.cs
+++ C# Questions/Folder.cs
@@ -1,4 +1,19 @@
-﻿using System;
+/*
+Implement a function FolderNames, which accepts a string containing an XML file that specifies folder structure and returns all folder names that start with startingLetter. The XML format is given in the example below.
+
+For example, for the letter 'u' and XML file:
+
+<?xml version="1.0" encoding="UTF-8"?>
+<folder name="c">
+    <folder name="program files">
+        <folder name="uninstall information" />
+    </folder>
+    <folder name="users" />
+</folder>
+the function should return "uninstall information" and "users" (in any order).
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Xml.Linq;
 
